6939555fa2a3f7a9575d9c36da2edff37501f265
Lebogang Ntlatleng <37046490+NtlatlengL@users.noreply.github.com>
2022-10-06 08:51:26 +0200

    Init Commit

---
+++ lib/feature/otp/presentation/otp_screen.dart
@@ -0,0 +1,56 @@
+import 'package:auth_ui/core/theme/app_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class OTPScreen extends StatelessWidget {
+  const OTPScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: AppColor.black,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top,
+          bottom: MediaQuery.of(context).padding.bottom,
+          left: 24,
+          right: 24,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color(0xFF11AAC1),
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart_outlined),
+              label: 'Buy',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.help_center_outlined),
+              label: 'Help',
+            ),
+          ]),
+    );
+  }
+}
